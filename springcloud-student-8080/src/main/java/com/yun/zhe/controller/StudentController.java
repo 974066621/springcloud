a98a3091c5206d6fa4f623e1c0f5d39d9370c627
liuyunzhe <974066621@qq.com>
2019-08-23 14:22:35 +0800

    配置中心

--- springcloud-student-8080/src/main/java/com/yun/zhe/controller/StudentController.java
+++ springcloud-student-8080/src/main/java/com/yun/zhe/controller/StudentController.java
@@ -1,5 +1,6 @@
 package com.yun.zhe.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,11 +9,15 @@ import com.yun.zhe.service.StudentService;
 
 @RestController
 public class StudentController {
-    
+    @Value("${info.app.name}")
+	private String value;
 	@GetMapping("/student/get")
 	public Student test() {
 		return new Student().setName("student:yunzhe");
 	}
-	
-	
+	@GetMapping("/student/info")
+	public String info() {
+		
+		return this.value;
+	}
 }
