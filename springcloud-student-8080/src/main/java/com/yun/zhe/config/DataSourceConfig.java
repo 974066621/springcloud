dc396e9e3d9d1c8e4fef33541f883bb770d2eac3
liuyunzhe <974066621@qq.com>
2019-08-24 09:59:51 +0800

    配置jdbcTemplate

---
+++ springcloud-student-8080/src/main/java/com/yun/zhe/config/DataSourceConfig.java
@@ -0,0 +1,15 @@
+package com.yun.zhe.config;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DataSourceConfig {
+	/**
+	 * 配置jdbcTemplate
+	 */
+	
+	
+	
+	
+}
--- springcloud-student-8080/src/main/java/com/yun/zhe/controller/StudentController.java
+++ springcloud-student-8080/src/main/java/com/yun/zhe/controller/StudentController.java
@@ -1,12 +1,11 @@
 package com.yun.zhe.controller;
 
-import java.util.Optional;
-
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.yun.zhe.dao.StudentDao;
 import com.yun.zhe.entity.Student;
 @RestController
@@ -14,7 +13,9 @@ public class StudentController {
 	
 	@Autowired
 	StudentDao studentDao;
-    @Value("${info.app.name}")
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+	@Value("${info.app.name}")
 	private String value;
 	@GetMapping("/student/get")
 	public Student get() {
@@ -27,4 +28,12 @@ public class StudentController {
 		
 		return this.value;
 	}
+	@GetMapping("/student/jdbctest")
+	public Student testJdbc() {
+		String sql="select * from student";
+		Map<String ,Object> map=jdbcTemplate.queryForMap(sql);
+		map.get("id");
+		return new Student().setId((Integer)map.get("id")).setName((String)map.get("name"));
+	}
+	
 }
